0f8896ed670bb06b51ecb2acc1754bf4267f2d99
TimWF8 <TimWF8@gmail.com>
2017-05-10 15:30:04 -0400

    Starts a Player Class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 public class Main {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.setSize(1000, 500);
-		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setVisible(true);
 	}
---
+++ src/Player.java
@@ -0,0 +1,22 @@
+
+public class Player {
+	
+	private int gold = 10;
+	private String name;
+	private int tanks;
+	
+	public Player(String Name){
+		name = Name;
+	}
+	public Player(){
+		name = "";
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int amtGold(){
+		return gold;
+	}
+}
