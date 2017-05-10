30933d062a5f6d5c453b5596e73c880838569425
TimWF8 <TimWF8@gmail.com>
2017-05-10 11:46:24 -0400

    Set up Eclipse project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(1000, 500);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
+}
