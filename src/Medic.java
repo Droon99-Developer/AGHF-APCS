4dbe7c17f6a77f61eae4ee2441cbc5b7b630e202
Julie1113 <julie1113@gmail.com>
2017-05-18 09:07:46 -0400

    Creates Medic class

---
+++ src/Medic.java
@@ -0,0 +1,31 @@
+
+public class Medic extends Unit{
+	private Unit linker;
+	private int healAmt = 2;
+	private int cost = 12;
+	private int GPK = 4;
+	private int speed = linker.getSpeed();
+	private int health = linker.getHealth();
+	public Medic(Unit attachment){
+		linker = attachment;
+	}
+	public int getSpeed() {
+		return super.getSpeed();
+	}
+
+	public int getHealth() {
+		return super.getHealth();
+	}
+
+	public int getStrength() {
+		return super.getStrength();
+	}
+	
+	public int getCost(){
+		return super.getCost();
+	}
+	
+	public int getPerKill(){
+		return super.getPerKill();
+	}
+}
